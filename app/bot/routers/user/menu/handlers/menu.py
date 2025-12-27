490b5ec3be9739f4a41410bb750a96876a1a56f9
Alex <aliakseych@gmail.com>
2025-12-27 16:59:13 +0500

    fix: make menu only accesable in private messages, assign /main and /menu commands to it

--- app/bot/routers/user/menu/handlers/menu.py
+++ app/bot/routers/user/menu/handlers/menu.py
@@ -4,11 +4,13 @@ from app.bot.routers.user.menu.router import router
 from app.bot.routers.user.menu.keyboard import menu as menu_kb
 from app.bot.utilities import reply_edit
 
+from aiogram import F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 
 
-@router.message(Command(commands=["start"]))
+@router.message(Command(commands=["start", "menu", "main"]),
+                F.chat.type.in_({"private"}))
 @router.callback_query(Menu.filter())
 async def menu(request: Message | CallbackQuery, container: Container):
     user_id = request.from_user.id
