45689c68fbcd9e5119f79d21f56b543d085ff184
Alex <aliakseych@gmail.com>
2026-01-13 16:09:22 +0500

    fix: change reply_edit.answer behaviour (ability to answering with message to request, even if it was a callback query)

--- app/bot/utilities/reply_edit.py
+++ app/bot/utilities/reply_edit.py
@@ -1,7 +1,10 @@
 from aiogram.types import CallbackQuery, Message
 
 
-async def answer(request: Message | CallbackQuery, **arguments):
+async def answer(request: Message | CallbackQuery, answer_: bool = False, **arguments):
+    if answer_ and isinstance(request, CallbackQuery):
+        await request.message.answer(**arguments)
+
     if isinstance(request, Message):
         await request.answer(**arguments)
     elif isinstance(request, CallbackQuery):
